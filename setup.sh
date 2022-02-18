32a0a539e51259a578b3611ce9a60efddee89b8b
Lucy McPhail <lucy.mcphail.p@gmail.com>
2022-02-18 18:26:18 +0000

    Remove setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-stow --dotfiles emacs
-stow --dotfiles git
-stow --dotfiles lisp
-stow --dotfiles python
-stow --dotfiles tmux
-stow --dotfiles vim
-stow --dotfiles yabai
-stow --dotfiles zsh
